c987ce775b04c3c6697aedef8a33c9490f27afc1
sarvesh123305 <sarvesh123305@gmail.com>
2023-03-05 08:50:51 +0530

    readme updated

--- exec.sh
+++ exec.sh
@@ -1,3 +1,3 @@
 clear
-gcc  main.c infixEvaluation.c linkedList.c addition.c characterStack/stack.c numberStack/stack.c subtraction.c multiplication.c divisionOptimized.c modulus.c power.c
+gcc main.c infixEvaluation.c linkedList.c addition.c characterStack/stack.c numberStack/stack.c subtraction.c multiplication.c divisionOptimized.c modulus.c power.c
 ./a.out
